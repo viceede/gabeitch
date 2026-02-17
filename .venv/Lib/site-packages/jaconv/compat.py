bbceb0c71198e438faa2dc58bc1938b23c2a78b3
viceede <d4rell1@yandex.ru>
2026-02-17 20:45:15 +0300

    updated dependencies

---
+++ .venv/Lib/site-packages/jaconv/compat.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from sys import version_info
+
+if version_info < (3,):
+    from itertools import imap, izip
+
+    map = imap
+    zip = izip
+    from codecs import open
+
+    open = open
+else:
+    map = map
+    zip = zip
+    open = open
